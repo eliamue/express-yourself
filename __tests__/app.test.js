59318bc96bd5980a1d5f18b09b0aebcac8516a90
Elise Muellerleile <eliamue@live.com>
2021-07-29 16:20:53 -0500

    getById test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import VillagersService from '../lib/services/VillagersService.js';
 
 describe('demo CRUD routes', () => {
   beforeEach(() => {
@@ -16,7 +17,6 @@ describe('demo CRUD routes', () => {
       species: 'Squirrel',
       gender: 'Female',
       catchphrase: 'slacker'
-
     };
     const res = await request(app)
       .post('/api/v1/villagers')
@@ -24,5 +24,21 @@ describe('demo CRUD routes', () => {
 
     expect(res.body).toEqual({ ...villager, id:'1' });
   });
+
+  it('tests get villager by id route', async () => {
+    const marshal = await VillagersService.generateVillager({
+      vid: 372,
+      name: 'Marshal',
+      personality: 'Smug',
+      species: 'Squirrel',
+      gender: 'Male',
+      catchphrase: 'sulky'
+    });
+    const res = await request(app)
+      .get(`/api/v1/villagers/${marshal.id}`);
+
+    expect(res.body).toEqual(marshal);
+  });
+
 });
 
