ca797a7302a3d3520ec07a0ae44cdad55ffa2847
Elise Muellerleile <eliamue@live.com>
2021-07-29 17:51:36 -0500

    getById model added, debugged id not being found issue, getById test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 import VillagersService from '../lib/services/VillagersService.js';
+// import Villagers from '../lib/models/Villagers.js';
 
 describe('demo CRUD routes', () => {
   beforeEach(() => {
@@ -26,18 +27,19 @@ describe('demo CRUD routes', () => {
   });
 
   it('tests get villager by id route', async () => {
-    const marshal = await VillagersService.generateVillager({
+    const marshal = {
       vid: 372,
       name: 'Marshal',
       personality: 'Smug',
       species: 'Squirrel',
       gender: 'Male',
       catchphrase: 'sulky'
-    });
+    };
+    await VillagersService.generateVillager({ vid: 372 });
     const res = await request(app)
-      .get(`/api/v1/villagers/${marshal.id}`);
+      .get('/api/v1/villagers/1');
 
-    expect(res.body).toEqual(marshal);
+    expect(res.body).toEqual({ ...marshal, id: '1' });
   });
 
 });
