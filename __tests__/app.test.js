8703ed43e91dfa73be8ec7e4d95a38e747d36afa
Elise Muellerleile <eliamue@live.com>
2021-07-29 11:33:36 -0500

    create route test framework, files for thing created

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,18 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('demo CRUD routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('tests create route', async () => {
+    const thing = { body: thing };
+    const res = await request(app)
+      .post('/api/v1/thing')
+      .send(thing);
+
+    expect(res.body).toEqual(thing);
+  });
 });
+
